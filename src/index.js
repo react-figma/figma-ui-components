d7df149c5cd42d875f5a4e5659464ecebd2ade4d
Ilia Lesik <ilialesik@gmail.com>
2019-08-02 13:49:54 +0300

    Configure build

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+export { PrimaryButton } from "./components/button/PrimaryButton";
+export { PrimaryDestructiveButton } from "./components/button/PrimaryDestructiveButton";
+export { SecondaryButton } from "./components/button/SecondaryButton";
+export { SecondaryDestructiveButton } from "./components/button/SecondaryDestructiveButton";
