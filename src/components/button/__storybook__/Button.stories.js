d07b42acb064ae432572a68edf91362d5dd50ced
Ilia Lesik <ilialesik@gmail.com>
2019-08-02 14:35:11 +0300

    Remove storybook files from build

--- src/components/button/__storybook__/Button.stories.js
+++ src/components/button/__storybook__/Button.stories.js
@@ -1,18 +1,17 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Button } from "./Button";
-import { PrimaryButton } from "./PrimaryButton";
-import { TextStyleDecorator } from "../../storybook/TextStylesDecorator";
+import { Button } from "../Button";
+import { PrimaryButton } from "../PrimaryButton";
+import { TextStyleDecorator } from "../../../__storybook__/TextStylesDecorator";
 import { boolean, withKnobs } from "@storybook/addon-knobs";
-import { PrimaryDestructiveButton } from "./PrimaryDestructiveButton";
-import { SecondaryButton } from "./SecondaryButton";
-import { SecondaryDestructiveButton } from "./SecondaryDestructiveButton";
+import { PrimaryDestructiveButton } from "../PrimaryDestructiveButton";
+import { SecondaryButton } from "../SecondaryButton";
+import { SecondaryDestructiveButton } from "../SecondaryDestructiveButton";
 
 storiesOf("Button", module)
     .addDecorator(TextStyleDecorator)
     .addDecorator(withKnobs)
-    .add("Button", () => <Button>Label</Button>)
     .add("PrimaryButton", () => <PrimaryButton disabled={boolean("disabled")}>Label</PrimaryButton>)
     .add("PrimaryDestructiveButton", () => (
         <PrimaryDestructiveButton disabled={boolean("disabled")}>Label</PrimaryDestructiveButton>
